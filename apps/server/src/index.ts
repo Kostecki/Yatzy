7ccbeea9e52932b4437cc5089f514b8c7c16b68e
Jacob Kostecki <jacob@kostecki.dk>
2026-07-19 00:00:50 +0200

    bootstrap fastify

---
+++ apps/server/src/index.ts
@@ -0,0 +1,24 @@
+import Fastify from "fastify";
+
+const PORT = Number(process.env.PORT) || 3000;
+
+const fastify = Fastify({
+	logger: true,
+});
+
+await fastify.register(import("@fastify/websocket"));
+
+// Declare a route
+fastify.get("/", (_request, reply) => {
+	reply.send({ hello: "world" });
+});
+
+// Run the server!
+fastify.listen({ host: "0.0.0.0", port: PORT }, (err) => {
+	if (err) {
+		fastify.log.error(err);
+		process.exit(1);
+	}
+
+	// Server is now listening on ${address}
+});
